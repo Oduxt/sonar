ce98eb2e29c69930a77a1e7e3a697c45d5a94a2c
Oduxt <93281983+Oduxt@users.noreply.github.com>
2021-10-29 15:19:35 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -3,8 +3,53 @@ distance = 0
 def on_forever():
     global distance
     distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
-    if distance < 10:
-        basic.show_icon(IconNames.NO)
-    else:
+    if distance < 25:
+        basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    elif distance < 20:
+        basic.show_leds("""
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    elif distance < 15:
+        basic.show_leds("""
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    elif distance < 10:
+        basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        """)
+    elif distance < 5:
+        basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        """)
+    elif distance < 0:
+        basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
         basic.show_icon(IconNames.YES)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,9 +2,54 @@ let distance = 0
 basic.forever(function on_forever() {
     
     distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
-    if (distance < 10) {
-        basic.showIcon(IconNames.No)
-    } else {
+    if (distance < 25) {
+        basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    } else if (distance < 20) {
+        basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    } else if (distance < 15) {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    } else if (distance < 10) {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+    } else if (distance < 5) {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+    } else if (distance < 0) {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
         basic.showIcon(IconNames.Yes)
     }
     
